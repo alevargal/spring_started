9cc9a81431a279512176b6ab2973bfc1f32d8031
maxwell <APetrunenko@alfabank.ru>
2021-09-14 17:15:54 +0300

    add new class

--- src/test/java/org/example/Test4.java
+++ src/test/java/org/example/Test4.java
@@ -10,16 +10,18 @@ public class Test4 {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext2.xml");
 
-        Dog myDog = context.getBean("myPet", Dog.class);
-        myDog.setName("Belka");
-        Dog yourDog = context.getBean("myPet", Dog.class);
-        yourDog.setName("Strelka");
-
-        System.out.println(myDog.getName());
-        System.out.println(yourDog.getName());
+//        Dog myDog = context.getBean("myPet", Dog.class);
+//        myDog.setName("Belka");
+//        Dog yourDog = context.getBean("myPet", Dog.class);
+//        yourDog.setName("Strelka");
+//
+//        System.out.println(myDog.getName());
+//        System.out.println(yourDog.getName());
 
 //        System.out.println("Переменные ссылаются на один и тот же обьект ? " + (myDog==yourDog));
 //        System.out.println(myDog);
 //        System.out.println(yourDog);
+
+        context.close();
     }
 }
